89ee4c5f6b1fdd9cbacffcafc79d74896f03e331
Glen1986 <escobar.glen@gmail.com>
2022-02-22 14:43:12 -0300

    lazyLoad

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "wouter";
-
 import "./styles.css";
 
 export default function Category({ name, options = [] }) {
@@ -9,11 +8,11 @@ export default function Category({ name, options = [] }) {
       <h3 className="Category-title">{name}</h3>
       <ul className="Category-list">
         {options.map((singleOption) => (
-          <li key={singleOption} className="Category-list-item">
+          <ul key={singleOption} className="Category-list-item">
             <Link className="Category-link" to={`/search/${singleOption}`}>
               {singleOption}
             </Link>
-          </li>
+          </ul>
         ))}
       </ul>
     </div>
--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -1,12 +1,35 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useRef} from 'react'
 import getTrendigTerms from '../../services/getTrendingsTermsService'
 import Category from '../Category'
 
-export default function TrendingSearches () {
+ function TrendingSearches () {
 	const [trends, setTrends] = useState([])
 	useEffect(() => {
     getTrendigTerms().then(setTrends)
 
 }, [])
 return <Category name='Tendencias' options = {trends}/>
+}
+
+
+export default function LazyTrending () {
+	const [show, setShow] = useState(false)
+  const elementRef = useRef()
+
+	useEffect(()=>{
+	const onChange = (entries, observer) =>{
+		const el = entries[0]
+		console.log(el.isIntersecting)
+		if(el.isIntersecting){
+		setShow(true)
+		observer.disconnect()
+		}
+	}
+
+	const observer = new IntersectionObserver(onChange, {
+	rootMrgin: '100px'
+		})
+		observer.observe(elementRef.current)
+	})
+	return <div ref = {elementRef}>{show ? <TrendingSearches /> : null}</div>
 }
