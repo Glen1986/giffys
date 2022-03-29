77726ff4a3e393b2498f243d3555f169fcde8200
Glen1986 <escobar.glen@gmail.com>
2022-03-29 11:34:39 -0300

    hooks-useform

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,57 +1,32 @@
-import React, { useReducer } from 'react'
+import React from 'react'
 import { useLocation } from 'wouter'
+import  useForm from './hooks'
 import './style.css'
+
 const RATINGS = ['g', 'pg', 'pg-13', 'r' ]
-
-const ACTIONS = {
-  UPDATE_KEYWORD: 'update_keyword',
-  UPDATE_RATING: 'update_rating'
-}
-
-const reducer = (state, action) => {
-switch (action.type){
-case ACTIONS.UPDATE_KEYWORD:
-    return {
-      ...state,
-      keyword:action.payload, 
-      times: state.times + 1
-}
-case ACTIONS.UPDATE_RATING:
-  return{
-    ...state,
-    rating: action.payload
-      }
-    default:
-  return state
-  }
-}
 
 function SearchForm ({initialRating = 'g', initialKeyword = ''}) {
 
-const [state, dispatch] = useReducer(reducer, {
-   keyword: decodeURIComponent(initialKeyword),
-   rating: initialRating,
-   times: 0
-})
-const { keyword, times, rating } = state
+const { keyword, times, rating, updateKeyword, updateRating } = useForm({
+initialKeyword,
+initialRating
+}) 
 
 const [_, pushLocation] =  useLocation()
 
-
 const handleChange = (evt) => {
- dispatch({type:ACTIONS.UPDATE_KEYWORD, payload: evt.target.value})
+ updateKeyword(evt.target.value)
 }
+
 const handleSubmit = (evt) => {
   evt.preventDefault()
   pushLocation(`/search/${keyword}/${rating}?`)
 }
 
 const handleChangeRating = evt => {
-   dispatch({type: ACTIONS.UPDATE_RATING, payload: evt.target.value})
+   updateRating(evt.target.value)
    setTimes(times + 1)
 }
-
-
   return(
     <>
        <form onSubmit={handleSubmit}>
